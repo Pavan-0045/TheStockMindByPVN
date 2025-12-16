137346aa32f83df57329f85d35cc5d84715cfed2
Pavan-0045 <pavan_mp.starsoftech@outlook.com>
2025-12-16 10:52:17 +0530

    Initialize the init folder

---
+++ thestockmind/src/main/java/com/thestockmind/thestockmind/ThestockmindApplication.java
@@ -0,0 +1,13 @@
+package com.thestockmind.thestockmind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThestockmindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThestockmindApplication.class, args);
+	}
+
+}
